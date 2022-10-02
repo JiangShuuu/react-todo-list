2099f734bb8622de1f38e65f2d2e670717dc0be2
JiangShuuu <globelex65@gmail.com>
2022-10-02 17:13:07 +0800

    add css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import React, {useState} from 'react';
 
 function App() {
+  
+  // Tasks (ToDo List) State
+  const [toDo, setToDo] = useState([
+    {"id": 1, "title": "Task 1", "status": true},
+    {"id": 2, "title": "Task 2", "status": false}
+  ]);
+
+  // Temp State
+  const [newTask, setNewTask] = useState('');
+  const [updateDate, setUpdateData] = useState('');
+
+  // Add task
+  const addTask = () => {
+    //
+  }
+
+  // Delete task
+  const deleteTask = (id) => {
+    //
+  }
+
+  // Mark task as done or completed
+  const markDone = (id) => {
+    //
+  }
+
+
+  // Cancel update
+  const cancelUpdate = () => {
+    //
+  }
+
+  // Chanfe task for update
+  const changeTask = (e) => {
+    //
+  }
+
+  // Update task
+  const updateTask = () => {
+    //
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <br /><br />
+      <h2>Todo-List App (ReactJS)</h2>
+      <br /><br />
+
+      {/* Display Todos */}
+
+      {toDo && toDo.length ? '' : 'No Task...'}
+
+      {toDo && toDo.map( (task, index) => {
+        return(
+          <React.Fragment key={task.id}>
+            <div className='col taskBg'>
+              <div className={task.status ? 'done' : ''}>
+                <span className="taskNumber">{index + 1}</span>
+                <span className="taskText">{task.title}</span>
+              </div>
+            </div>
+          </React.Fragment>
+        )})
+      }
     </div>
   );
 }
